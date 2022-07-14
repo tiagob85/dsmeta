411ab94e956d473c7a2a66d03b3018fd9b0978ba
Tiago <tiago.gpes@gmail.com>
2022-07-14 00:33:41 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         </section>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
